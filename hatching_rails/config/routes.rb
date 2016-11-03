6639ca4beee280a4d0eb3063b1d0e5b4938c9ef6
nsemmler <ndsunc@gmail.com>
2016-11-03 16:59:22 -0400

    removed merge conflicts in routes and db/schema

--- hatching_rails/config/routes.rb
+++ hatching_rails/config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
+  resources :users
 
   get '/user'  => 'users#new'
+
   post '/user'  => 'users#create'
 
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  post '/create' => 'Eggs#create'
-  get '/show' => 'Eggs#show'
-  put '/edit' => 'Eggs#update'
->>>>>>> 5a6acb4b12f5285b41af45c609889d0de1c77f43
-=======
   post '/create' => 'eggs#create'
+
   get '/show' => 'eggs#show'
+
   put '/edit' => 'eggs#update'
->>>>>>> fab19e25c26d35a9c9ddb0af53ed2c249dc4f784
 end
