d1c7707a610a570ade1f2193417fedc793dfd724
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-03 15:08:15 -0400

    no user eg gin that table

--- hatching_rails/app/controllers/eggs_controller.rb
+++ hatching_rails/app/controllers/eggs_controller.rb
@@ -1,2 +1,33 @@
 class EggsController < ApplicationController
+  def create
+    @egg = Egg.new egg_params
+    if @egg.save
+      result = {}
+      result[:total] = @egg.total
+      render json: result
+    else
+      result = {
+        ok: false,
+        error: @egg.error.full_messages
+      }
+      render json: { ok: false }, status: 422
+    end
+  end
+
+  def show
+    @show_eggs = Egg.total
+    inventory = {}
+    inventory[:total] = @show_eggs.total
+    render json: inventory
+  end
+
+  def update(@egg)
+    @users_get = @egg - params[:users_eggs]
+  end
+
+  private
+
+  def egg_params
+    params.require(:list).permit(:total)
+  end
 end
--- hatching_rails/db/migrate/20161103183740_create_eggs.rb
+++ hatching_rails/db/migrate/20161103183740_create_eggs.rb
@@ -2,8 +2,6 @@ class CreateEggs < ActiveRecord::Migration[5.0]
   def change
     create_table :eggs do |t|
       t.integer :total
-      t.integer :user_eggs
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
