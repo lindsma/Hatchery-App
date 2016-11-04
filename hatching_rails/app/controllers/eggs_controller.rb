4ff9c7fe826a523991e67a1fbf75f93a30584f14
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-04 09:45:53 -0400

    egg controller working

--- hatching_rails/app/controllers/eggs_controller.rb
+++ hatching_rails/app/controllers/eggs_controller.rb
@@ -1,36 +1,31 @@
 class EggsController < ApplicationController
   def create
-    # @egg = Egg.new egg_params
-    # @egg.total += params[:input]
-    # if @egg.save
-    #   result = {}
-    #   result[:total] = @egg.total
-    #   render json: result
-    # else
-    #   result = {}
-    #   render json: result
-    # end
-    @egg = Egg.new egg_params
-    result = {}
-    result[:total] = @egg.total
-    render json: result
+    @egg = Egg.new(total: params[:input].to_i)
+    if @egg.save
+      result = {}
+      result[:total] = @egg.total
+      render json: result
+    else
+      result = {}
+      render json: result
+    end
   end
 
   def show
-    @show_eggs = Egg.total
+    @egg = Egg.where(id: 1).first
     inventory = {}
-    inventory[:total] = @show_eggs.total
+    inventory[:total] = @egg.total
     render json: inventory
   end
 
   def update
-    puts params
-    if current_user.rank == "admin"
-      #if user.admin true
-      @egg.total += params[:input]
-    else
-      @egg.total = @egg.total - params[:input]
-      user.eggs += params[:input]
+    # # if current_user.rank == "admin"
+    # #  if user.admin true
+       @egg = Egg.where(id: 1).first
+    #   @egg.total += params[:input].to_i
+    # else
+      @egg.total = @egg.total - params[:input].to_i
+      #user.eggs += params[:input].to_i
     end
     edited = {}
     edited[:total] = @egg.total
