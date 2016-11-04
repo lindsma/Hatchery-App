62fb4b6eee412eb1771121f3992726cefe7411e6
nsemmler <ndsunc@gmail.com>
2016-11-04 12:30:31 -0400

    removed token authentication, added admin boolean to assign the first user who signs into the application as the administrator

--- hatching_rails/db/schema.rb
+++ hatching_rails/db/schema.rb
@@ -10,23 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103204109) do
+ActiveRecord::Schema.define(version: 20161103184524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "auth_token"
 
   create_table "eggs", force: :cascade do |t|
     t.integer  "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
