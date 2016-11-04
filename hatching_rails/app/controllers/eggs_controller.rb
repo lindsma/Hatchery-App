1d2e5b92e2f5dcf447e88c10e012e7c9362520ac
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-04 12:26:13 -0400

    egg controller simple done

--- hatching_rails/app/controllers/eggs_controller.rb
+++ hatching_rails/app/controllers/eggs_controller.rb
@@ -19,22 +19,33 @@ class EggsController < ApplicationController
   end
 
   def update
-    # # if current_user.rank == "admin"
-    # #  if user.admin true
-       @egg = Egg.where(id: 1).first
-    #   @egg.total += params[:input].to_i
-    # else
+    if current_user.admin true
+      @egg = Egg.where(id: 1).first
+      @egg.total = @egg.total + params[:input].to_i
+      @egg.save
+      edited = {}
+      edited[:total] = @egg.total
+      edited[:user_eggs] = 0
+      render json: edited
+    else
       @egg.total = @egg.total - params[:input].to_i
-      #user.eggs += params[:input].to_i
+      if @egg.user_eggs != Integer
+        @egg.user_eggs = params[:input].to_i
+        @egg.save
+      else
+        @egg.user_eggs + params[:input].to_i
+        @egg.save
+      end
+      edited = {}
+      edited[:total] = @egg.total
+      edited[:user_eggs] = @egg.user_eggs
+      render json: edited
     end
-    edited = {}
-    edited[:total] = @egg.total
-    render json: edited
   end
 
   private
 
   def egg_params
-    params.require(:list).permit(:total)
+    params.require(:egg).permit(:total)
   end
 end
