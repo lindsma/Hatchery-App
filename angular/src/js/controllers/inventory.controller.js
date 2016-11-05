a9f807659f33c0463ff725c2f646c62cdf277304
Marian Powell <marianpowell@gmail.com>
2016-11-05 13:20:38 -0400

    added welcome admin div

--- angular/src/js/controllers/inventory.controller.js
+++ angular/src/js/controllers/inventory.controller.js
@@ -16,19 +16,19 @@ angular.module('hatcheryApp')
   this.submitEggs = function(number){
   InventoryService.sendJSON({"total": number})
   animateEggs(number);
-  // .success(function(body){
-  //   console.log(body.number);
-  //   })
-  console.log('submitEggs number: ' + number);
+  .success(function(body){
+    console.log(body.number);
+    })
+  // console.log('submitEggs number: ' + number);
   }
 
   /*this.editEggs calls the service function editJSON to put a JSON object*/
   this.editEggs = function(editNum){
   InventoryService.sendJSON({input: editNum})
-  // .success(function(body){
-  //   console.log(body.editNum);
-  // })
-  console.log('editEggs number: ' + editNum);
+  .success(function(body){
+    console.log(body.editNum);
+  })
+  // console.log('editEggs number: ' + editNum);
   }
 
   function animateEggs(number){
