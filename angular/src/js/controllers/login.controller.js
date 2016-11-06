5ce0b21d2527c3bbfd28abab00ab64eddcf2b945
Marian Powell <marianpowell@gmail.com>
2016-11-06 13:53:52 -0500

    mixins file added. responsiveness lacking

--- angular/src/js/controllers/login.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('hatcheryApp').controller('LoginController', function() {
-
-console.log('in login');
-
-});
--- angular/src/js/controllers/register.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('hatcheryApp').controller('RegisterController', function() {
-
-console.log('in register');
-
-});
