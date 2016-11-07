737787431572e3c6cfea7e7e53de2184c5f65b90
Marian Powell <marianpowell@gmail.com>
2016-11-07 15:34:36 -0500

    log in functioning properly.

--- angular/src/js/controllers/account.controller.js
+++ angular/src/js/controllers/account.controller.js
@@ -1,22 +1,28 @@
 
 angular.module('hatcheryApp').controller('AccountController', function($scope, accountService, $state, InventoryService) {
-  $scope.loggedIn = {id: 1, username: "lindsma", email: "linzma87@gmail.com", password_digest: "$2a$10$WiiUKw0ruD7HM6Hd9Eo0yuK/74XsHFSwCQhc65jZjZo4IjhHX8jFq", admin: false};
+  // $scope.loggedIn = {id: 0, username: "lindsma", email: "linzma87@gmail.com", password_digest: "$2a$10$WiiUKw0ruD7HM6Hd9Eo0yuK/74XsHFSwCQhc65jZjZo4IjhHX8jFq", admin: false};
   this.orders = $scope.getOrders || [{"id":"id","user":"username","eggs":0,"date":Date.now()}];
+  $scope.loggedIn={};
   /* this.getEggs calls the service function getJSON to return a JSON object. The number returned is displayed as current eggs available.*/
   $scope.getEggs = InventoryService.getJSON(function(response){
     $scope.currentEggs = response.data || 0;
-    console.log($scope.currentEggs.total);
     $state.reload;
   })
 
     $scope.checkLogin = function(username) {
       accountService.get(username, function(response) {
         $scope.loggedIn = response.data;
+        console.log($scope.loggedIn.admin);
+        goToPage($scope.loggedIn);
       });
-
-      $state.go('hatcheryParent.home');
     };
-
+    function goToPage(userObj){
+      if(userObj.admin === true){
+        $state.go('hatcheryParent.admin');
+      }else {
+        $state.go('hatcheryParent.home');
+      }
+    }
 
     $scope.processForm = function(username, email, password) {
         $scope.formData = {
